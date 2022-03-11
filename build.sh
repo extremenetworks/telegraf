583d161a1545190ada794f4edfdd21ecf55ad922
David Carson <dcarson@extremenetworks.com>
2022-03-11 14:27:56 -0500

    convenience wrapper script for building/uploading telegraf utility

---
+++ build.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Simple ExtremeNetworks script to facilitate building and
+# uploading the telegraf utility.
+
+set -e
+
+usage()
+{
+    echo "usage: $0 arch {build | upload}"
+    echo " . arch  : valid architectures: arm64, x86_64, mips"
+    echo " . build : build and tar utility for specified architecture"
+    echo " . upload: upload specified architecture's tar to Artifactory"
+}
+
+build()
+{
+    make clean
+    rm -f ${target}
+    make CGO_ENABLED=0 GOOS=linux GOARCH=${bld_arch} GOPROXY=https://proxy.golang.org,direct GOSUMDB=sum.golang.org
+    tar -cf ${target} telegraf
+    rm -f telegraf
+}
+
+upload()
+{
+    if [ ! -f ${target} ]; then
+        echo "info: ${target} not found; building first..."
+        build
+        if [ ! -f ${target} ]; then
+            echo "error: could not find or build '${target}' tarball"
+            exit 1
+        fi
+    fi
+
+    # make sure jfrog config is set up
+    if ! jfrog config show ${salem} &>/dev/null ; then
+        echo "Could not find Salem Artifactory config.  Let's create one..."
+        echo "Accept defaults and use your corporate password."; echo ""
+        jfrog config add --url http://engartifacts1.extremenetworks.com:8081 --user $(whoami) ${salem}
+        if ! jfrog config show ${salem} &>/dev/null ; then
+            echo "error: failed to configure ${salem} Artifactory server access"
+            exit 1
+        fi
+    fi
+    jfrog rt upload ${target} xos-binaries-local-release/telegraf/${target} --server-id ${salem}
+}
+
+#######################
+# execution starts here
+#######################
+if [[ -z "$1" || "$1" == "--help" || "$1" == "-h" || "$1" == "?" ]]; then
+    usage
+    exit 0
+fi
+
+# force go to use alternate location of modules
+go env -w GOMODCACHE=/opt/go/pkg/mod
+go env -w GOCACHE=$(pwd -P)/.cache/go-build
+# grab version string
+version=$(cat build_version.txt)
+salem=Salemm
+
+# set architecture name and build architecture as used by golang
+arch=$1
+if [ "$arch" = "x86_64" ]; then
+    bld_arch=amd64
+else
+    bld_arch=${arch}
+fi
+target=telegraf.${arch}-${version}.tar
+
+# check action argument
+case $2 in
+    build | upload)
+        action=$2
+        ;;
+    *)
+        echo "error: invalid action '$2'"
+        usage
+        exit 1
+esac
+
+# perform action
+case $1 in
+    arm64 | mips | x86_64)
+        $action
+        ;;
+    *)
+        echo "error: invalid architecture '$1'"
+        usage
+        exit 1
+esac
+
