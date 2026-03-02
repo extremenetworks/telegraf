89bd6106b054ca1f0b57cfd132cbde4ebbfa7799
Borigi Harshith Gupta <bgupta@extremenetworks.com>
2026-03-02 04:06:21 -0800

    HOS-24883 Update description and values for StaLeaveStatsTrapElement

--- plugins/inputs/ah_trap/ah_trap_defines_common.go
+++ plugins/inputs/ah_trap/ah_trap_defines_common.go
@@ -165,6 +165,105 @@ func clientMacProtoToString(proto int32) string {
 	}
 }
 
+func eventReasonCodeToString(code uint32) string {
+	switch code {
+	case 0:
+		return "IDLE_TIMEOUT"
+	case 1:
+		return "SESSION_TIMEOUT"
+	case 2:
+		return "ADMIN_DEAUTH"
+	case 3:
+		return "ASSOC_FAILED"
+	case 4:
+		return "AUTH_FAILED"
+	case 5:
+		return "AUTHZ_FAILED"
+	case 6:
+		return "ROAMING"
+	case 7:
+		return "SESSION_TIMEOUT_2"
+	case 8:
+		return "AP_INITIATED_DISCONNECT"
+	case 9:
+		return "CLIENT_INITIATED_DISCONNECT"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func eventTypeToString(eventType uint32) string {
+	switch eventType {
+	case 0:
+		return "CLIENT_CONNECT"
+	case 1:
+		return "CLIENT_DISCONNECT"
+	case 2:
+		return "CLIENT_ROAM"
+	case 3:
+		return "CLIENT_FAILURE"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func reasonCodeToString(code uint32) string {
+	switch code {
+	case 0:
+		return "RESERVED"
+	case 1:
+		return "UNSPECIFIED"
+	case 2:
+		return "PREV_AUTH_NOT_VALID"
+	case 3:
+		return "STA_LEAVING_IBSS_ESS"
+	case 4:
+		return "INACTIVITY"
+	case 5:
+		return "AP_UNABLE_TO_HANDLE"
+	case 6:
+		return "CLASS2_FRAME_FROM_NONAUTH_STA"
+	case 7:
+		return "CLASS3_FRAME_FROM_NONASSOC_STA"
+	case 8:
+		return "STA_LEAVING_BSS"
+	case 9:
+		return "STA_NOT_AUTHENTICATED"
+	case 10:
+		return "POWER_CAPABILITY_UNACCEPTABLE"
+	case 11:
+		return "SUPPORTED_CHANNELS_UNACCEPTABLE"
+	case 12:
+		return "RESERVED_12"
+	case 13:
+		return "INVALID_IE"
+	case 14:
+		return "MIC_FAILURE"
+	case 15:
+		return "FOURWAY_HANDSHAKE_TIMEOUT"
+	case 16:
+		return "GROUP_KEY_HANDSHAKE_TIMEOUT"
+	case 17:
+		return "IE_IN_FOURWAY_DIFFERENT"
+	case 18:
+		return "INVALID_GROUP_CIPHER"
+	case 19:
+		return "INVALID_PAIRWISE_CIPHER"
+	case 20:
+		return "INVALID_AKMP"
+	case 21:
+		return "UNSUPPORTED_RSN_IE_VERSION"
+	case 22:
+		return "INVALID_RSN_IE_CAPABILITIES"
+	case 23:
+		return "IEEE_802_1X_AUTH_FAILED"
+	case 24:
+		return "CIPHER_SUITE_REJECTED"
+	default:
+		return "UNKNOWN"
+	}
+}
+
 type AhFaMvlanChangeTrap struct {
 	TrapType      uint8
 	SystemID      [10]uint8
